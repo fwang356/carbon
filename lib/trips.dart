fcd03cbcb876b9af619085bed171c081b3937fdd
Eric Mesina <esmesina02@gmail.com>
2021-10-23 20:08:02 -0400

    Attempt at drives query

--- lib/trips.dart
+++ lib/trips.dart
@@ -1,4 +1,7 @@
 // @dart=2.9
+import 'package:carbon/loading.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'map.dart';
@@ -13,175 +16,192 @@ class TripPage extends StatefulWidget {
 class _TripState extends State<TripPage> {
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    Future<QuerySnapshot<Map<String, dynamic>>> trips = firestore
+        .collection("users")
+        .doc(auth.currentUser.uid)
+        .collection("drives")
+        .get();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
             "Car-Bon: Recent Trips"),
       ),
       body: Center(
-          child: ListView(
-              children: <Widget>[
-                const Padding(
-                    padding: EdgeInsets.only(top: 12, left: 12, right: 12),
-                    child: Text(
-                        "Day 1",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w700
-                        )
-                    )
-                ),
+        child: FutureBuilder(
+            future: trips,
+            builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+              if(snapshot.data != null) {
+                print(snapshot.data.docs);
+              }
+              return ListView(
+                    children: <Widget>[
+                      const Padding(
+                          padding: EdgeInsets.only(top: 12, left: 12, right: 12),
+                          child: Text(
+                              "Day 1",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700
+                              )
+                          )
+                      ),
 
-                Padding(
-                  padding: const EdgeInsets.only(top: 12, bottom: 12, left: 12, right: 12),
-                  child: Card(
-                    elevation: 5,
-                    color: const Color(0xFFFFFFFF),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        const Padding(
-                            padding: EdgeInsets.only(top: 12, right: 12, left: 12, bottom: 12),
-                            child: Text("Distance: 30 Miles \n\nCarbon Emissions: 8.8 kg")
-                        ),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 12, bottom: 12, left: 12, right: 12),
+                          child: Card(
+                              elevation: 5,
+                              color: const Color(0xFFFFFFFF),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Row(
+                                  children: <Widget>[
+                                    const Padding(
+                                        padding: EdgeInsets.only(top: 12, right: 12, left: 12, bottom: 12),
+                                        child: Text("Distance: 30 Miles \n\nCarbon Emissions: 8.8 kg")
+                                    ),
 
-                        Padding(
-                          padding: const EdgeInsets.only(left: 40),
-                          child: ButtonBar(
-                              alignment: MainAxisAlignment.end,
-                              children: [
-                                ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => const MapPage())
-                                      );
-                                    },
-                                    child: const Text("View Route",
-                                        style: TextStyle(
-                                            fontSize: 14
-                                        )),
-                                    style: ElevatedButton.styleFrom(
-                                      primary: const Color(0xFF7badab),
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(40)),
-                                      minimumSize: const Size(60, 40),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 40),
+                                      child: ButtonBar(
+                                          alignment: MainAxisAlignment.end,
+                                          children: [
+                                            ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(builder: (context) => const MapPage())
+                                                  );
+                                                },
+                                                child: const Text("View Route",
+                                                    style: TextStyle(
+                                                        fontSize: 14
+                                                    )),
+                                                style: ElevatedButton.styleFrom(
+                                                  primary: const Color(0xFF7badab),
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(40)),
+                                                  minimumSize: const Size(60, 40),
+                                                )
+                                            )
+                                          ]
+                                      ),
                                     )
-                                )
-                              ]
-                          ),
-                        )
-                      ]
-                    )
-                  )
-                ),
-
-                Padding(
-                    padding: const EdgeInsets.only(top: 12, bottom: 12, left: 12, right: 12),
-                    child: Card(
-                        elevation: 5,
-                        color: const Color(0xFFFFFFFF),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Row(
-                            children: <Widget>[
-                              const Padding(
-                                  padding: EdgeInsets.only(top: 12, right: 12, left: 12, bottom: 12),
-                                  child: Text("Distance: 30 Miles \n\nCarbon Emissions: 8.8 kg")
-                              ),
-
-                              Padding(
-                                padding: const EdgeInsets.only(left: 40),
-                                child: ButtonBar(
-                                    alignment: MainAxisAlignment.end,
-                                    children: [
-                                      ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(builder: (context) => const MapPage())
-                                            );
-                                          },
-                                          child: const Text("View Route",
-                                              style: TextStyle(
-                                                  fontSize: 14
-                                              )),
-                                          style: ElevatedButton.styleFrom(
-                                            primary: const Color(0xFF7badab),
-                                            shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.circular(40)),
-                                            minimumSize: const Size(60, 40),
-                                          )
-                                      )
-                                    ]
-                                ),
+                                  ]
                               )
-                            ]
-                        )
-                    )
-                ),
+                          )
+                      ),
 
-                const Padding(
-                    padding: EdgeInsets.only(top: 12, left: 12, right: 12),
-                    child: Text(
-                        "Day 2",
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700
-                        )
-                    )
-                ),
-
-                Padding(
-                    padding: const EdgeInsets.only(top: 12, bottom: 12, left: 12, right: 12),
-                    child: Card(
-                        elevation: 5,
-                        color: const Color(0xFFFFFFFF),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Row(
-                            children: <Widget>[
-                              const Padding(
-                                  padding: EdgeInsets.only(top: 12, right: 12, left: 12, bottom: 12),
-                                  child: Text("Distance: 30 Miles \n\nCarbon Emissions: 8.8 kg")
+                      Padding(
+                          padding: const EdgeInsets.only(top: 12, bottom: 12, left: 12, right: 12),
+                          child: Card(
+                              elevation: 5,
+                              color: const Color(0xFFFFFFFF),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
                               ),
+                              child: Row(
+                                  children: <Widget>[
+                                    const Padding(
+                                        padding: EdgeInsets.only(top: 12, right: 12, left: 12, bottom: 12),
+                                        child: Text("Distance: 30 Miles \n\nCarbon Emissions: 8.8 kg")
+                                    ),
 
-                              Padding(
-                                padding: const EdgeInsets.only(left: 40),
-                                child: ButtonBar(
-                                    alignment: MainAxisAlignment.end,
-                                    children: [
-                                      ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.push(
-                                                context,
-                                                MaterialPageRoute(builder: (context) => const MapPage())
-                                            );
-                                          },
-                                          child: const Text("View Route",
-                                              style: TextStyle(
-                                                  fontSize: 14
-                                              )),
-                                          style: ElevatedButton.styleFrom(
-                                            primary: const Color(0xFF7badab),
-                                            shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.circular(40)),
-                                            minimumSize: const Size(60, 40),
-                                          )
-                                      )
-                                    ]
-                                ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 40),
+                                      child: ButtonBar(
+                                          alignment: MainAxisAlignment.end,
+                                          children: [
+                                            ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(builder: (context) => const MapPage())
+                                                  );
+                                                },
+                                                child: const Text("View Route",
+                                                    style: TextStyle(
+                                                        fontSize: 14
+                                                    )),
+                                                style: ElevatedButton.styleFrom(
+                                                  primary: const Color(0xFF7badab),
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(40)),
+                                                  minimumSize: const Size(60, 40),
+                                                )
+                                            )
+                                          ]
+                                      ),
+                                    )
+                                  ]
                               )
-                            ]
-                        )
-                    )
-                ),
-              ]
-          )),
+                          )
+                      ),
+
+                      const Padding(
+                          padding: EdgeInsets.only(top: 12, left: 12, right: 12),
+                          child: Text(
+                              "Day 2",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700
+                              )
+                          )
+                      ),
+
+                      Padding(
+                          padding: const EdgeInsets.only(top: 12, bottom: 12, left: 12, right: 12),
+                          child: Card(
+                              elevation: 5,
+                              color: const Color(0xFFFFFFFF),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Row(
+                                  children: <Widget>[
+                                    const Padding(
+                                        padding: EdgeInsets.only(top: 12, right: 12, left: 12, bottom: 12),
+                                        child: Text("Distance: 30 Miles \n\nCarbon Emissions: 8.8 kg")
+                                    ),
+
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 40),
+                                      child: ButtonBar(
+                                          alignment: MainAxisAlignment.end,
+                                          children: [
+                                            ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(builder: (context) => const MapPage())
+                                                  );
+                                                },
+                                                child: const Text("View Route",
+                                                    style: TextStyle(
+                                                        fontSize: 14
+                                                    )),
+                                                style: ElevatedButton.styleFrom(
+                                                  primary: const Color(0xFF7badab),
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(40)),
+                                                  minimumSize: const Size(60, 40),
+                                                )
+                                            )
+                                          ]
+                                      ),
+                                    )
+                                  ]
+                              )
+                          )
+                      ),
+                    ]
+                );
+            }),
+      )
     );
   }
 }
